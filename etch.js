7ce87fbe4860777f8f784fe311fc1a07a9dafe67
shields <117888554+shieldzzz@users.noreply.github.com>
2023-03-15 14:19:57 -0700

    Put JS logic in a function to accept the desired number of squares as a parameter and generate the grid dynamically

--- etch.js
+++ etch.js
@@ -1,17 +1,20 @@
-let board = document.querySelector('.board');
-board.style.display = 'grid';
-board.style.gridTemplateColumns = 'repeat(32, 1fr)';
-board.style.gridTemplateRows = 'repeat(32, 1fr)';
+function makeGrid(numSquare) { 
+    let board = document.querySelector('.board');
+    board.style.display = 'grid';
+    board.style.gridTemplateColumns = `repeat(${numSquare}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${numSquare}, 1fr)`;
 
-for (let i = 0; i < 1024; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'white';
-    square.style.width = 'auto';
-    square.style.height = 'auto';
-    square.style.border = '1px solid black';
-    board.insertAdjacentElement('beforeend', square);
+    for (let i = 0; i < (numSquare * numSquare); i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'white';
+        square.style.width = 'auto';
+        square.style.height = 'auto';
+        square.style.border = '1px solid black';
+        board.appendChild(square);
+    }
 }
 
+makeGrid(16);
 
 
 
