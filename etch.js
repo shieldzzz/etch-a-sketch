e2a5da2cc8409a1f1f97f0688eda5125585becec
shields <117888554+shieldzzz@users.noreply.github.com>
2023-03-25 14:06:50 -0700

    Refactor code to get grid size by slider choice instead of prompt, add functionality to color picker button, remove shade button functionality

--- etch.js
+++ etch.js
@@ -1,20 +1,31 @@
-const adjustBtn = document.getElementById('adjustSize');
 const randomBtn = document.getElementById('randomColor');
 const blackBtn = document.getElementById('blackButton');
 const clearBtn = document.getElementById('clearGrid');
-const shadeBtn = document.getElementById('shadeButton');
+const sizeRange = document.getElementById('myRange');
+const sizeRangeValue = document.getElementById('rangeValue');
+const colorPicker = document.getElementById('colorPicker');
 
-adjustBtn.addEventListener('click', () => {
-    let newSize = prompt('Enter a whole number between 16 and 100.');
+colorPicker.addEventListener('click', (event) => {
     let squares = document.querySelectorAll('.board div');
     squares.forEach((square) => {
-        square.style.backgroundColor = '#f7f7f7';
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = event.target.value;
+        });
     });
-    if (newSize >= 16 && newSize <= 100 && Number.isInteger(Number(newSize))) {
-        makeGrid(newSize);
-    } else {
-        alert('Please enter a valid number between 16 and 100');
-    }
+});
+
+sizeRangeValue.textContent = sizeRange.value;
+sizeRange.addEventListener('input', () => {
+    let newSize = sizeRange.value;
+    sizeRangeValue.textContent = newSize;
+    let squares = document.querySelectorAll('.board div');
+    makeGrid(newSize);
+    squares.forEach((square) => {
+        square.style.backgroundColor = '#f7f7f7';
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+    });
 });
 
 randomBtn.addEventListener('click', () => {
@@ -42,21 +53,7 @@ clearBtn.addEventListener('click', () => {
     });
 });
 
-shadeBtn.addEventListener('click', () => {
-    let squares = document.querySelectorAll('.board div');
-    squares.forEach((square) => {
-        let shadeCount = 0;
-        square.addEventListener('mouseover', () => {
-            if (shadeCount < 10) {
-                shadeCount++;
-                let opacity = 0.1 * shadeCount;
-                square.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-            }
-        });
-    });
-});
-
-function makeGrid(numSquare = 16) { 
+function makeGrid(numSquare) { 
     let board = document.querySelector('.board');
     board.style.display = 'grid';
     board.style.gridTemplateColumns = `repeat(${numSquare}, 1fr)`;
@@ -75,5 +72,4 @@ function makeGrid(numSquare = 16) {
     }
 }
 
-makeGrid();
-
+makeGrid(16);
